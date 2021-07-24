07da29c8c34e4cc8d057edfb6f016c43d79da668
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-07-24 08:20:01 +0300

    Initial commit

---
+++ northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -0,0 +1,41 @@
+package kodlamaio.northwind.core.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+
+    @Column(name = "email")
+    @Email
+    @NotBlank
+    @NotNull
+    private String  email;
+
+
+    @Column(name = "password")
+    @NotBlank
+    @NotNull
+
+    private String password;
+
+}
